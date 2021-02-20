fe74683a3c474113ade0e294f0109e42b76c60ff
yurikokaito <79347784+yurikokaito@users.noreply.github.com>
2021-02-20 12:10:01 -0500

    Update
    Support multiple feeds for a single company

--- rss_checker.py
+++ rss_checker.py
@@ -14,25 +14,36 @@ def find_inactive(rss_feed_urls, days_of_inactive):
     cutoff_time = time.time() - (days_of_inactive * DAY_IN_SECONDS)
     latest_publishing_times = {}
 
-    # Obtain the publishing time of the latest published item of each feed
+    # obtain the publishing time of the latest published item of each feed
     for company in rss_feed_urls:
-        rss_feed = feedparser.parse(rss_feed_urls[company])
-        latest_publishing_time = None
+        # check if value is array (multiple feeds)
+        urls = rss_feed_urls[company]
+        if isinstance(urls, list) is False:
+            urls =[urls]
 
-        for entry in rss_feed.entries:
-            publishing_time = entry.get('published_parsed', None)
-            
-            if publishing_time is not None:
-                publishing_time = calendar.timegm(publishing_time)  # Specify publishing time as a timestamp
+        for url in urls:
+            rss_feed = feedparser.parse(url)
+            latest_publishing_time = None
 
-                if latest_publishing_time is None:
-                    latest_publishing_time = publishing_time
-                elif publishing_time > latest_publishing_time:
-                    latest_publishing_time = publishing_time
+            for entry in rss_feed.entries:
+                publishing_time = entry.get('published_parsed', None)
+                
+                if publishing_time is not None:
+                    publishing_time = calendar.timegm(publishing_time)  # specify publishing time as a timestamp
 
-        latest_publishing_times[company] = latest_publishing_time
+                    if latest_publishing_time is None:
+                        latest_publishing_time = publishing_time
+                    elif publishing_time > latest_publishing_time:
+                        latest_publishing_time = publishing_time
 
-    # Obtain the list of inactive companies within days_of_inactive days
+            # update latest_publishing_time if it is alreay in the Dictionaries
+            publishing_time = latest_publishing_times.get(company)
+            if publishing_time is None:
+                latest_publishing_times[company] = latest_publishing_time
+            elif latest_publishing_time >  publishing_time:
+                latest_publishing_times[company] = latest_publishing_time
+
+    # obtain the list of inactive companies within days_of_inactive days
     inactive_companies = []
     for company in latest_publishing_times:
         latest_publishing_time = latest_publishing_times[company]
--- test_rss_checker.py
+++ test_rss_checker.py
@@ -7,12 +7,16 @@ DAY_IN_SECONDS = 60 * 60 * 24
 class TestRssChecker(unittest.TestCase):
     def test_find_inactive(self):
         from rss_checker import find_inactive
+
+        # it should return no company name with a given number of days
         feed_urls_by_companies = {}
         with self.subTest(feed_urls_by_companies=feed_urls_by_companies):
             self.assertEqual([], find_inactive(feed_urls_by_companies, 0))
             self.assertEqual([], find_inactive(feed_urls_by_companies, 1))
             self.assertEqual([], find_inactive(feed_urls_by_companies, 99))
 
+        # it should return no company name if a given number of days sets earler than Feb 16 2021
+        # it should return 'apology_line' if a given number of days sets later than Feb 16 2021
         feed_urls_by_companies = {'apology_line': 'test_rss/apology_line.xml'}
         with self.subTest(feed_urls_by_companies=feed_urls_by_companies):
             # time stamp of the latest item
@@ -25,6 +29,26 @@ class TestRssChecker(unittest.TestCase):
             self.assertEqual([], find_inactive(feed_urls_by_companies, days_elapsed_apology_line + 1))
             self.assertEqual(['apology_line'], find_inactive(feed_urls_by_companies, days_elapsed_apology_line - 1))
 
+        # it should return 'apology_line' if a given number of days sets later than Feb 16 2021
+        # it should return no company name if a given number of days sets 1 day later than Feb 14 2021
+        feed_urls_by_companies = {'apology_line': ['test_rss/apology_line.xml', 'test_rss/apology_line2.xml']}
+        with self.subTest(feed_urls_by_companies=feed_urls_by_companies):
+            # time stamp of the latest item
+            apology_line_latest = calendar.timegm((2021, 2, 16, 8, 5, 0, 1, 47, 0))
+            apology_line_latest2 = calendar.timegm((2021, 2, 14, 8, 5, 0, 1, 47, 0)) 
+            
+            # time elapsed since the latest item
+            time_elapsed_apology_line= time.time() - apology_line_latest
+            time_elapsed_apology_line2 = time.time() - apology_line_latest2
+            
+            days_elapsed_apology_line = time_elapsed_apology_line // DAY_IN_SECONDS
+            self.assertEqual(['apology_line'], find_inactive(feed_urls_by_companies, days_elapsed_apology_line - 1))
+
+            days_elapsed_apology_line2 = time_elapsed_apology_line2 // DAY_IN_SECONDS          
+            self.assertEqual([], find_inactive(feed_urls_by_companies, days_elapsed_apology_line2 - 1))
+
+        # it should return no company name if a given number of days sets earler than Feb 16 2021
+        # it should return 'apology_line' and 'lincoln_project' if a given number of days sets later than Feb 17 2021
         feed_urls_by_companies = {'apology_line': 'test_rss/apology_line.xml',
                                   'lincoln_project': 'test_rss/lincoln_project.xml'}
         with self.subTest(feed_urls_by_companies=feed_urls_by_companies):
@@ -42,6 +66,7 @@ class TestRssChecker(unittest.TestCase):
             days_elapsed_lincoln_project = time_elapsed_lincoln_project // DAY_IN_SECONDS
             self.assertEqual(['apology_line', 'lincoln_project'], find_inactive(feed_urls_by_companies, days_elapsed_lincoln_project - 1))
 
+        # it should return all company names if a given number of days sets today
         feed_urls_by_companies = {'apology_line': 'test_rss/apology_line.xml',
                                   'lincoln_project': 'test_rss/lincoln_project.xml',
                                   'politico-news': 'test_rss/politico-news.xml'}
